2b3db644173702290335156a0d1073cab12299b9
Gustav Grahn <ggrahn@kth.se>
2025-02-11 14:13:01 +0100

    added functions and declarations

--- Homework3/src/main.c
+++ Homework3/src/main.c
@@ -9,4 +9,37 @@
 #include <sys/time.h>  
 #include <semaphore.h>
 
+#define MAXWOMEN 10 /* maximum number of women */
+#define MAXMEN 10 /* maximum number of men */
+#define MAXVISITS 50 /* maximum number of visits */
 
+sem_t *bathroom_access;
+sem_t *women_queue;
+sem_t *men_queue;
+
+int current_women_in_bath = 0;
+int current_men_in_bath = 0;
+int waiting_women = 0;
+int waiting_men = 0;
+
+bool women_exit_phase;
+bool mean_exit_phase;
+
+int numberOfWomen = 0;
+int numberOfMen = 0;
+int numberOfVisitis = 0;
+
+void *Women(void *arg)
+{
+
+}
+
+void *Men(void *arg) 
+{
+
+}
+
+int main ()
+{
+    
+}
