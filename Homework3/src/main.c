290c99b7a76684af6c3bf137db27ac21f4606502
Gustav Grahn <ggrahn@kth.se>
2025-02-11 14:51:19 +0100

    added women function

--- Homework3/src/main.c
+++ Homework3/src/main.c
@@ -31,7 +31,7 @@ int numberOfVisits = 0;
 
 void *Women(void *arg)
 {
-    long id = (int)arg;
+    long id = (long)arg;
     for (int i = 0; i < numberOfVisits; i++) 
     {
         /* Do different things before entering*/
@@ -55,7 +55,33 @@ void *Women(void *arg)
         {
             sem_v(bathroom_access);
         }
+
+        /* Women goes to the bathroom */
+        printf("Woman %ld enters the bathroom. Visit: %d\n", id, i + 1);
+        usleep((rand() % 3000) * 1000);
+
+        /* After finished bathroom visit */
+        sem_p(bathroom_access);
+        current_women_in_bath--;
+
+        /* Make sure men will enter after a women went to the bathroom */
+        women_exit_phase = true;
+        if (current_women_in_bath == 0) {
+            women_exit_phase = false;
+        }
+
+        /* Prio men for fairness */
+        if (current_women_in_bath == 0 && waiting_men > 0)
+        {
+            waiting_men--;
+            sem_v(men_queue);
+        }
+        else
+        {
+            sem_v(bathroom_access);
+        }
     }
+    pthread_exit(NULL);
 }
 
 void *Men(void *arg) 
