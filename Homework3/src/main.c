e66686d788a594392c99cf685ee0f9651a832f58
Mauro Lindberg Oyarzo <maurolo@kth.se>
2025-02-14 08:26:48 +0100

    klar inshallah

--- Homework3/src/main.c
+++ Homework3/src/main.c
@@ -8,14 +8,15 @@
 #include <pthread.h>
 #include <sys/time.h>  
 #include <semaphore.h>
+#include <unistd.h>
 
 #define MAXWOMEN 10 /* maximum number of women */
 #define MAXMEN 10 /* maximum number of men */
 #define MAXVISITS 50 /* maximum number of visits */
 
-sem_t *bathroom_access;
-sem_t *women_queue;
-sem_t *men_queue;
+sem_t bathroom_access;
+sem_t women_queue;
+sem_t men_queue;
 
 int current_women_in_bath = 0;
 int current_men_in_bath = 0;
@@ -29,31 +30,32 @@ int numberOfWomen = 0;
 int numberOfMen = 0;
 int numberOfVisits = 0;
 
+
 void *Women(void *arg)
 {
-    long id = (long)arg;
+    long id = (long)(intptr_t)arg;
     for (int i = 0; i < numberOfVisits; i++) 
     {
         /* Do different things before entering*/
         usleep((rand() % 4000) * 1000);
 
         /* Check if the bathroom is occupied; if so, wait */
-        sem_p(bathroom_access);
+        sem_wait(&bathroom_access);
         if (current_men_in_bath > 0 || women_exit_phase) 
         {
             waiting_women++;
-            sem_v(bathroom_access);
-            sem_p(women_queue);
+            sem_post(&bathroom_access);
+            sem_wait(&women_queue);
         }
         current_women_in_bath++;
         if (waiting_women > 0)
         {
             waiting_women--;
-            sem_v(women_queue);
+            sem_post(&women_queue);
         }
         else
         {
-            sem_v(bathroom_access);
+            sem_post(&bathroom_access);
         }
 
         /* Women goes to the bathroom */
@@ -61,7 +63,7 @@ void *Women(void *arg)
         usleep((rand() % 3000) * 1000);
 
         /* After finished bathroom visit */
-        sem_p(bathroom_access);
+        sem_wait(&bathroom_access);
         current_women_in_bath--;
 
         /* Make sure men will enter after a women went to the bathroom */
@@ -74,11 +76,11 @@ void *Women(void *arg)
         if (current_women_in_bath == 0 && waiting_men > 0)
         {
             waiting_men--;
-            sem_v(men_queue);
+            sem_post(&men_queue);
         }
         else
         {
-            sem_v(bathroom_access);
+            sem_post(&bathroom_access);
         }
     }
     pthread_exit(NULL);
@@ -86,32 +88,32 @@ void *Women(void *arg)
 
 void *Men(void *arg)
 {
-    long id = (long) arg;
+    long id = (long)(intptr_t) arg;
     int i;
     for(i = 0; i < numberOfVisits; i++){
 
         usleep((rand() % 4000) * 1000);
 
-        sem_wait(bathroom_access);
+        sem_wait(&bathroom_access);
         if(current_women_in_bath > 0 || men_exit_phase){
             waiting_men += 1;
-            sem_post(bathroom_access);
-            sem_wait(men_queue);
+            sem_post(&bathroom_access);
+            sem_wait(&men_queue);
         }
         current_men_in_bath++;
         if(waiting_men >0){
             waiting_men--;
-            sem_post(men_queue);
+            sem_post(&men_queue);
         } else{
-            sem_post(bathroom_access);  
+            sem_post(&bathroom_access);  
         }
 
-        printf( "a MAN enters the bathroom. Visit number: ","%d\n", i+1);
+        printf( "man number %ld enters the bathroom. Visit number:  %d\n",id,  i+1);
         
         /* do something else after they've been to the bathroom*/
         usleep((rand() % 3000) * 1000);
 
-        sem_wait(bathroom_access);
+        sem_wait(&bathroom_access);
         current_men_in_bath--;
         men_exit_phase = true;
 
@@ -119,9 +121,9 @@ void *Men(void *arg)
 
         if(current_men_in_bath == 0 && waiting_women >0){
             waiting_women--;
-            sem_post(women_queue);
+            sem_post(&women_queue);
         } else {
-            sem_post(bathroom_access);
+            sem_post(&bathroom_access);
         }
 
 
@@ -156,9 +158,9 @@ int main()
 	pthread_t threads_men[numberOfMen], threads_women[numberOfWomen];
 	int i;
 	for (i = 0; i < numberOfMen; i++)
-		pthread_create(&threads_men[i], NULL, Men, (void *)i);
+		pthread_create(&threads_men[i], NULL, Men, (void *)(intptr_t)i);
 	for (i = 0; i < numberOfWomen; i++)
-		pthread_create(&threads_women[i], NULL, Women, (void *)i);
+		pthread_create(&threads_women[i], NULL, Women, (void *)(intptr_t)i);
 
 	for (i = 0; i < numberOfMen; i++)
 		pthread_join(threads_men[i], NULL);
