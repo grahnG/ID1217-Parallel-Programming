adb174dfc9b8a4cc99dbc84daa1f1bd578142ad0
Gustav Grahn <ggrahn@kth.se>
2025-02-24 10:06:23 +0100

    ring.c fully functional now

--- Homework5/src/ring.c
+++ Homework5/src/ring.c
@@ -25,7 +25,7 @@ double read_timer() {
 
 int main(int argc, char *argv[])
 {
-	int numberOfRounds = (argc > 1) ? atoi(argv[1]) : MAXNUMBEROFROUNDS
+	int numberOfRounds = (argc > 1) ? atoi(argv[1]) : MAXNUMBEROFROUNDS;
 
     int rank;
     int size;
@@ -35,6 +35,9 @@ int main(int argc, char *argv[])
 
     srand(time(NULL) * rank);
 
+    int prev_rank = (rank - 1 + size) % size; // Prevoius process in the ring
+    int next_rank = (rank + 1) % size; // Next process in the ring
+
     double start_time;
     double end_time;
     if (rank == STARTER) {
@@ -43,6 +46,57 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < numberOfRounds; i++) {
         /* Initialize seed and get the random number */
-        int v = rand() % 100;
+        int my_value = rand() % 100;
+
+        int min_max_values[2]; // [0] -> max value, [1] -> min value
+
+        // Round 1: compute min and max in a pipeline
+        if (rank  == STARTER)
+        {
+            // Initialize with STARTER'S own value
+            min_max_values[0] = my_value; // Max
+            min_max_values[1] = my_value; // Min
+
+            printf("\n[STARTER] Starting ring with value: %d\n", my_value);
+
+            // STARTER sends intial min/max value to the next process
+            MPI_Send(&min_max_values, 2, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
+        }
+
+        // Receive min/max from the previous process
+        MPI_Recv(&min_max_values, 2, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Update min/max based on my_value
+        min_max_values[0] = (my_value > min_max_values[0]) ? my_value : min_max_values[0]; // Max
+        min_max_values[1] = (my_value < min_max_values[1]) ? my_value : min_max_values[1]; // Min
+
+        // Forward updated values to the next process
+        MPI_Send(&min_max_values, 2, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
+
+        printf("[P%d] My value: %d | Max so far: %d | Min so far: %d\n", rank, my_value, min_max_values[0], min_max_values[1]);
+
+        // Round 2: Distribute final min/max across the ring
+        MPI_Recv(&min_max_values, 2, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Forward to the next process (except last one)
+        if (rank < size - 1)
+        {
+            MPI_Send(&min_max_values, 2, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
+        }
+        printf("[P%d] Received final min/max | Max: %d | Min: %d\n", rank, min_max_values[0], min_max_values[1]);
     }
+
+    if (rank == STARTER)
+    {
+        end_time = read_timer();
+    }
+
+    MPI_Finalize();
+
+    if (rank == STARTER) 
+    {
+        printf("%d rounds took %g seconds\n", numberOfRounds, end_time - start_time);
+    }
+
+    return 0;
 }
