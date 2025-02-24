19dbd0b7b8887914b56e7c077a605525539b7d24
Gustav Grahn <ggrahn@kth.se>
2025-02-24 09:33:24 +0100

    added main

--- Homework5/src/centralized.c
+++ Homework5/src/centralized.c
@@ -7,6 +7,8 @@
 
 #define MAXNUMBEROFROUNDS 5
 
+#define STARTER 0
+
 double read_timer() {
     static struct timeval start;
     static int initialized = 0;
@@ -19,4 +21,28 @@ double read_timer() {
 
     gettimeofday(&end, NULL);
     return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
+
+int main(int argc, char *argv[])
+{
+	int numberOfRounds = (argc > 1) ? atoi(argv[1]) : MAXNUMBEROFROUNDS
+
+    int rank;
+    int size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    srand(time(NULL) * rank);
+
+    double start_time;
+    double end_time;
+    if (rank == STARTER) {
+        start_time = read_timer();
+    }
+
+    for (int i = 0; i < numberOfRounds; i++) {
+        /* Initialize seed and get the random number */
+        int v = rand() % 100;
+    }
 }
--- Homework5/src/symmetric.c
+++ Homework5/src/symmetric.c
@@ -20,3 +20,27 @@ double read_timer() {
     gettimeofday(&end, NULL);
     return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
 }
+
+int main(int argc, char *argv[])
+{
+	int numberOfRounds = (argc > 1) ? atoi(argv[1]) : MAXNUMBEROFROUNDS
+
+    int rank;
+    int size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    srand(time(NULL) * rank);
+
+    double start_time;
+    double end_time;
+    if (rank == STARTER) {
+        start_time = read_timer();
+    }
+
+    for (int i = 0; i < numberOfRounds; i++) {
+        /* Initialize seed and get the random number */
+        int v = rand() % 100;
+    }
+}
