3baf8f6ffe55b0bae9c9e7784016b830e218f05c
Gustav Grahn <ggrahn@kth.se>
2025-02-24 10:14:53 +0100

    symmetric.c fully functional now

--- Homework5/src/symmetric.c
+++ Homework5/src/symmetric.c
@@ -43,6 +43,32 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < numberOfRounds; i++) {
         /* Initialize seed and get the random number */
-        int v = rand() % 100;
+        int my_value = rand() % 100;
+
+        /* Array to store value from all processes */
+        int all_values[size];
+
+        /* Each process sends its value to all others and receive everyone's values */
+        MPI_Allgather(&my_value, 1, MPI_INT, &all_values[0], 1, MPI_INT, MPI_COMM_WORLD);
+
+        // Compute the minimum and maximum values from all collected values
+        int min_value = my_value;
+        int max_value = my_value;
+
+        for (int i = 0; i < size; i++) {
+            if (all_values[i] > max_value) max_value = all_values[i];
+            if (all_values[i] < min_value) min_value = all_values[i];
+        }
+
+        // Print results for each process
+        printf("[Process %d] My value: %d | Max: %d | Min: %d\n", rank, my_value, max_value, min_value);
     }
+
+    if (rank == ROOT) {
+        end_time = read_timer();
+        printf("%d rounds took %g seconds\n", numberOfRounds, end_time - start_time);
+    }
+
+    MPI_Finalize();
+    return 0;
 }
